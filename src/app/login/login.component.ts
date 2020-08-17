6166f7c3f46817c1316d0234ce8280df186ba5cc
Samuel Bader <54846029+Samrhan@users.noreply.github.com>
2020-08-17 22:58:45 +0200

    Log avec création du cookie

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,30 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import {CookieService} from 'ngx-cookie-service';
 
-@Component({ templateUrl: 'login.component.html' })
-export class LoginComponent {}
+
+@Component({templateUrl: 'login.component.html'})
+export class LoginComponent implements OnInit {
+
+  constructor(private cookieService: CookieService) {
+  }
+
+  ngOnInit(): void {
+    const fragment = new URLSearchParams(window.location.hash.slice(1));
+    const token = fragment.get('access_token');
+    const tokenType = fragment.get('token_type');
+
+    fetch('https://discordapp.com/api/users/@me', {
+      headers: {
+        authorization: `${tokenType} ${token}`
+      }
+    })
+      .then(res => res.json())
+      .then(response => {
+        this.cookieService.set('user', response.id);
+      })
+      .catch(console.error);
+  }
+}
 
 
